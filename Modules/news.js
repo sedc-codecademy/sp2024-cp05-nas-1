1baf659cd23771bd04516d36b7d080ca37ee3e92
dnisko <dnisko@hotmail.com>
2024-05-30 22:25:28 +0200

    added news cards and implemented dummy api

---
+++ Modules/news.js
@@ -0,0 +1,22 @@
+export class News
+{
+    constructor(
+        {
+            id,
+            author,
+            title,
+            url,
+            source,
+            name,
+            urlToImage
+        })
+        {
+            this.id = id,
+            this.author = author,
+            this.title = title,
+            this.url = url,
+            this.source = source,
+            this.name = name,
+            this.urlToImage = urlToImage
+        }
+}
---
+++ app.js
@@ -0,0 +1,3 @@
+import { NewsService } from "./Modules/news-service.js";
+const newsService = new NewsService();
+newsService.registerEvents();
